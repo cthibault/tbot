58dece46913480c0e5d0ea12af28113cb3663da3
Curtis Thibault <cuthib@microsoft.com>
2017-02-06 22:51:37 -0800

    Hello World node webapp to test deployment

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-require('coffee-script/register');
-module.exports = require('hubot/bin/hubot.coffee');
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) 
+{
+res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.end('Hello World...');
+}).listen(port);
